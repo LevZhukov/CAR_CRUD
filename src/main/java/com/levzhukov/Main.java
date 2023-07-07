c1601173261047311a34bcf8abf10c9302d3c212
Lev Zhukov <lev33zhukov@gmail.com>
2023-07-07 14:15:50 +0200

    resolve conflicts

--- src/main/java/com/levzhukov/Main.java
+++ src/main/java/com/levzhukov/Main.java
@@ -5,12 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-<<<<<<< HEAD
-    public static void main(String[] args){
-        SpringApplication.run(Main.class, args);
-=======
     public static void main(String[] args) {
-        SpringApplication.run(Main.class);
->>>>>>> car
+        SpringApplication.run(Main.class, args);
     }
 }
